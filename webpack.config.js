aa73ceaa7511645fd5a092b6d47e0b3f2affa789
Nam <namph2102@gmail.com>
2023-06-11 07:19:33 +0700

    upload

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,10 @@ import Dotenv from "dotenv-webpack";
 
 module.exports = {
   plugins: [
-    new Dotenv({
-      path: "./some.other.env", // default is .env
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("development"),
+      },
     }),
   ],
 };
